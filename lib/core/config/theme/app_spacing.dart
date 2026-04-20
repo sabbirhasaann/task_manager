9e7a6edfb296aaf837bfd2d17c752bba61bf9794
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-04-20 19:54:16 +0600

    more clean AppGap

--- lib/core/config/theme/app_spacing.dart
+++ lib/core/config/theme/app_spacing.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 abstract class AppSpacing {
   static const double xs  = 4;
   static const double sm  = 8;
@@ -16,4 +18,28 @@ abstract class AppRadius {
   static const double lg   = 16;
   static const double xl   = 24;
   static const double full = 999;
+}
+
+
+abstract class AppGap {
+  // Vertical
+  static const hXS = SizedBox(height: AppSpacing.xs);
+  static const hSM = SizedBox(height: AppSpacing.sm);
+  static const hMD = SizedBox(height: AppSpacing.md);
+  static const hLG = SizedBox(height: AppSpacing.lg);
+  static const hXL = SizedBox(height: AppSpacing.xl);
+  static const hXLarge = SizedBox(height: AppSpacing.xlarge);
+  static const hXXL = SizedBox(height: AppSpacing.xxl);
+  static const hJumbo = SizedBox(height: AppSpacing.jumbo);
+
+  // Horizontal
+  static const wXS = SizedBox(width: AppSpacing.xs);
+  static const wSM = SizedBox(width: AppSpacing.sm);
+  static const wMD = SizedBox(width: AppSpacing.md);
+  static const wLG = SizedBox(width: AppSpacing.lg);
+  static const wXL = SizedBox(width: AppSpacing.xl);
+  static const wXLarge = SizedBox(width: AppSpacing.xlarge);
+  static const wXXL = SizedBox(width: AppSpacing.xxl);
+  static const wJumbo = SizedBox(width: AppSpacing.jumbo);
+
 }
