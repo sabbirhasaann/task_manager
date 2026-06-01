e7b7fb21f58fdfb7d1c86d6247631a4750a8c546
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-01 16:10:52 +0600

    register routes

--- lib/core/config/router/app_routes.dart
+++ lib/core/config/router/app_routes.dart
@@ -1,3 +1,6 @@
 class AppRoutes {
   static const String initial = '/';
+  static const String onboardingView = 'onboardingView/';
+  static const String mainWrapperView = 'mainWrapperView/';
+  static const String homeView = 'homeView/';
 }
--- lib/core/config/router/route_generator.dart
+++ lib/core/config/router/route_generator.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/material.dart';
 import 'app_routes.dart';
 import '../../../l10n/app_localizations.dart';
+
 // Import your screens here
+import 'package:task_manager/features/onboarding/presentation/pages/onboarding_view.dart';
+import 'package:task_manager/features/main_wrapper/presentation/pages/main_wrapper_view.dart';
+import 'package:task_manager/features/home/presentation/pages/home_view.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.initial:
         return MaterialPageRoute(builder: (_) => const HomeView());
+
+      case AppRoutes.onboardingView:
+        return MaterialPageRoute(
+          builder: (ctx) {
+            return const OnboardingView();
+          },
+        );
+      case AppRoutes.mainWrapperView:
+        return MaterialPageRoute(
+          builder: (ctx) {
+            return const MainWrapperView();
+          },
+        );
+      case AppRoutes.homeView:
+        return MaterialPageRoute(
+          builder: (ctx) {
+            return const HomeView();
+          },
+        );
       default:
         return _errorRoute();
     }
   }
 
   static Route<dynamic> _errorRoute() {
-    return MaterialPageRoute(builder: (_) {
-      return const Scaffold(
-        body: Center(child: Text('Route not found')),
-      );
-    });
+    return MaterialPageRoute(
+      builder: (_) {
+        return const Scaffold(body: Center(child: Text('Route not found')));
+      },
+    );
   }
 }
-
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -30,16 +52,8 @@ class HomeView extends StatelessWidget {
     final l10n = AppLocalizations.of(context)!;
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Text('Home View'),
-            Text(
-              l10n.helloWorld,
-            ),
-          ],
-        ),
+        child: Column(children: [Text('Home View'), Text(l10n.helloWorld)]),
       ),
     );
   }
 }
-
