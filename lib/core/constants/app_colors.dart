6134a30d99bbbdb3a74301a8876887bbb702b3aa
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-04 16:47:45 +0600

    color: register new color

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -8,4 +8,12 @@ class AppColors {
   static const Color green = Color(0xFF46F080);
   static const Color yellow = Color(0xFFEDF046);
   static const Color blue = Color(0xFF46BDF0);
+  static const Color pastelOrange = Color(0xFFFF9142);
+  static const Color lightPastelOrange = Color(0xFFFFE6D4);
+  static const Color azureRadiance = Color(0xFF0087FF);
+  static const Color lightAzuerRadiance = Color(0xFFE3F2FF);
+  static const Color purpleShade = Color(0xFF9260F4);
+  static const Color lightPurpleShade = Color(0xFFEDE4FF);
+  static const Color hotPink = Color(0xFFF478B8);
+  static const Color lightHotPink = Color(0xFFFFE4F2);
 }
